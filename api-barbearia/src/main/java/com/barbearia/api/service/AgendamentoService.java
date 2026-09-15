c1f5df31d218e05a777b38bfa5d08717f798e918
milen <milen@DESKTOP-344LMQ5>
2026-09-15 12:58:14 -0300

    feat: adiciona modulo mvc de barbeiro

--- api-barbearia/src/main/java/com/barbearia/api/service/AgendamentoService.java
+++ api-barbearia/src/main/java/com/barbearia/api/service/AgendamentoService.java
@@ -1,5 +1,23 @@
 package com.barbearia.api.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.barbearia.api.model.Agendamento;
+import com.barbearia.api.repository.AgendamentoRepository;
+
+@Service
 public class AgendamentoService {
 
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    public Agendamento salvar(Agendamento agendamento) {
+        return agendamentoRepository.save(agendamento);
+    }
+
+    public List<Agendamento> listarTodos() {
+        return agendamentoRepository.findAll();
+    }
 }
